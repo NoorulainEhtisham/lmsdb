42fccb0a43fd0539c21cb203dc3c51baaa1355c9
NoorulainEhtisham <noorulain.ehtisham@gmail.com>
2021-12-11 20:40:58 +0500

    date fixes

--- util.js
+++ util.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const moment = require('moment');
 
 const app = express();
 
-function arrayToJSON ( col, data){
+function arrayToJSON(col, data) {
 
     const finalArray = [];
-  
 
-    data.forEach((row, ri)=>{
-      var obj = {};
-      col.forEach((c, ci)=>{
-        obj[c] = row[ci];
-      })
-      finalArray.push(obj);
+
+    data.forEach((row, ri) => {
+        var obj = {};
+        col.forEach((c, ci) => {
+            if (c.includes('DATE') && row[ci] != null) {
+                obj[c] = moment(row[ci]).format('DD-MMM-YY');
+            }
+            else obj[c] = row[ci];
+        })
+        finalArray.push(obj);
     });
 
     return finalArray;
